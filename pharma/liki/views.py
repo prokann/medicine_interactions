27b5ac37aa394a036adbe09a6803629f6e448e67
prokann <annaaudia5@gmail.com>
2023-09-10 21:14:10 +0300

    Fixed category extraction, added composition extraction.

--- pharma/liki/views.py
+++ pharma/liki/views.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from .models import *
 
 
 s = Service('C:\chromedriver\chromedriver.exe')
@@ -30,27 +31,45 @@ def take_data():
             driver.switch_to.window(driver.window_handles[-1])
             time.sleep(2)
 
-            #do
+            # extract data from page
             name = driver.find_element(By.CSS_SELECTOR, 'h1.drug-card__title.drug-card__title_map').text
             real_name = re.split('\d+', name)[0].replace("®", "")[:-1]
             # name = driver.find_element(By.CSS_SELECTOR, 'h1.drug-card__title.drug-card__title_map').text.split(',')[0]
             print(real_name)
 
-            category = driver.find_elements(By.CSS_SELECTOR, 'a.breadcrumbs-site__list-link')[-1]
-            composition = driver.find_elements(By.XPATH,
-                                '//i[text()="діюча речовина: "]/following::p[following::b[text()="Лікарська форма"]]')
-            if not composition:
+            category = driver.find_elements(By.CSS_SELECTOR, 'a.breadcrumbs-site__list-link')[-1].text
+            print(category)
+
+            extracted_components = []
+            # try except is not necessary
+            try:
                 composition = driver.find_elements(By.XPATH,
-                                '//i[text()="діюча речовина:"]/following::p[following::b[text()="Лікарська форма"]]')
-            # composition = [i.text for i in composition]
-            # components = [i.text.split() for i in composition]
-            components = [item for sublist in [i.text.split() for i in composition] for item in sublist]
+                                            '//h2[@id="anchor1"]/following::p[following::b[text()="Лікарська форма"]]')
+                components = [i.text for i in composition]
+
+                for component in components:
+                    matches = re.findall(r'[.,;:]?\s*([\w\s\(\)\d%]+)[.,;:]?\s*', component)
+                    formatted_components = [match.strip('.,;:') for match in matches]
+                    extracted_components.extend(formatted_components)
+
+                for component in extracted_components:
+                    if 'містять' in component or 'містить' in component or 'допоміжні речовини' in component \
+                            or 'допоміжна речовина' in component or 'діюча речовина' in component \
+                            or 'діючі речовини' in component:
+                        extracted_components.remove(component)
+
+            except Exception as e:
+                print(e)
+
+            print(extracted_components)
+
+            # Medicaments(name=real_name, category=category, composition=extracted_components).save()
 
             driver.close()
-            time.sleep(2)
+            # time.sleep(2)
 
             driver.switch_to.window(driver.window_handles[0])
-            time.sleep(2)
+            time.sleep(1)
 
             elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, xpath_selector)))
 
@@ -61,9 +80,13 @@ def take_data():
     driver.quit()
 
 
-# while button_more:
-#     try:
-#         button_more.click()
-#     except Exception as e:
-#         break
+def compare_medicines():
+    pass
+
+
+while button_more:
+    try:
+        button_more.click()
+    except Exception as e:
+        break
 take_data()
